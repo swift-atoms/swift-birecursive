1e0f32b0e158a8f27b660a3fdf9fe32b204b74da
Coen ten Thije Boonkkamp <coen@coenttb.com>
2026-09-17 13:26:45 +0200

    Point dependencies at their new organizations (algebra packages in swift-atoms; interface and client in swift-molecules)

--- Package.swift
+++ Package.swift
@@ -9,8 +9,8 @@ let package = Package(
         .library(name: "Birecursive Macro Core", targets: ["Birecursive Macro Core"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/swift-molecules/swift-corecursive.git", branch: "main"),
-        .package(url: "https://github.com/swift-molecules/swift-recursive.git", branch: "main"),
+        .package(url: "https://github.com/swift-atoms/swift-corecursive.git", branch: "main"),
+        .package(url: "https://github.com/swift-atoms/swift-recursive.git", branch: "main"),
         .package(url: "https://github.com/swiftlang/swift-syntax.git", "603.0.2"..<"604.0.0"),
     ],
     targets: [
